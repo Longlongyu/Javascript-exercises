58f5bc3bba1349eb35538e9a50bae2d3839a0891
Longlongyu <longlongyuu@gmail.com>
2018-08-13 14:07:54 +0800

    Update:  js code rewrite.

--- javascript/game.js
+++ javascript/game.js
@@ -1,88 +1,116 @@
 function startGame(){
-  var compose = function(f, g) {return function() {return f(g.apply(g, arguments));};};
-  var dom = function(str) {return document.querySelector(str);};
-  var control = function(o, f) {return function(s) {return o[f](s);};};
-  var get = function(o, k) {return o[k];};
-  var set = function(o, k, s) {o[k] = s;};
-  var memorize = function(f) {var cache = {};
-    return function() {
-      var arg_str = JSON.stringify(arguments);
-      cache[arg_str] = cache[arg_str] || f.apply(f, arguments);
-      return cache[arg_str];
-    };
-  };
-
-  var get_dom = memorize(dom);
-  var get_dom_v = function(e, k) {return get(get_dom(e), k);};
-  var set_dom_v = function(e, k, s) {return set(get_dom(e), k, s);};
-  var set_dom_Attribute = function(e, k, s) {return get_dom(e).setAttribute(k, s);};
+  var _ = {
+    compose : function(f, g) {return function() {return f(g.apply(g, arguments))}},
+    control : function(o, f) {return function(s) {return o[f](s)}},
+    get : function(o, k) {return o[k]},
+    set : function(o, k, s) {return o[k] = s},
+    add : function(x, y) {return x + y},
+    memorize : function(f) {
+      var cache = {}
+      return function() {
+        var arg_str = JSON.stringify(arguments);
+        cache[arg_str] = cache[arg_str] || f.apply(f, arguments)
+        return cache[arg_str]
+      }
+    },
+    curry : function(fn) {
+      var limit = fn.length
+      return function judgeCurry (...args) {
+        if (args.length >= limit) {
+          return fn.apply(null, args)
+        } else {
+          return function(...args2) {
+            return judgeCurry.apply(null, args.concat(args2))                                     
+          }
+        }
+      }
+    }
+  }
+  
+  var Maybe = function(x) {this.__value = x}
+  Maybe.of = function(x) {return new Maybe(x)}
+  Maybe.prototype.isNothing = function() {return (this.__value === null || this.__value === undefined)}
+  Maybe.prototype.map = function(f) {return this.isNothing() ? Maybe.of(null) : Maybe.of(f(this.__value))}
+  
+  var dom = function(str) {return document.querySelector(str)}
+  var get_dom = _.memorize(dom)
+  var get_dom_v = function(e, k) {return _.get(get_dom(e), k)}
+  var set_dom_v = function(e, k, s) {return _.set(get_dom(e), k, s)}
+  var set_dom_Attribute = function(e, k, s) {return get_dom(e).setAttribute(k, s)}
+  var setListen = _.curry(function(f, ele) {return ele.addEventListener('click', f)}) 
   
   return {
     numberGuessGame : function() {
+      var disabled = _.curry(function(bool) {
+        set_dom_v('.GameSubmit', 'disabled', bool)
+        set_dom_v('.GameText', 'disabled', bool)
+      })
       
-      var get_random =  function(x) {return Math.floor(Math.random() * x) + 1};
-      
-      var switchDisabled = function(bool) {
-        set_dom_v('.GameSubmit', 'disabled', bool);
-        set_dom_v('.GameText', 'disabled', bool);
-      };
+      var create = function(x) {return Maybe.of(x)}
+      var getVal = _.curry(function(x) {return x.__value})
+      var setVal = _.curry(function(x, v) {return Maybe.of(x.__value = v)})
+      var map = _.curry(function(x, f) {return x.map(f)})
 
-      var game = {
-        turn : 0,
-        random : 0,
-        guessArray : [],
-        reStart : function() {
-          turn = 0;
-          random = get_random(100);
-          guessArray = [];
-          set_dom_v('.guess', 'textContent', '');
-          set_dom_v('.output', 'textContent', '');
-          set_dom_v('.confirm', 'textContent', '');
-          switchDisabled(false);
-        },
-        setGameOver : function() {
-          switchDisabled(true);
-          resetButton = document.createElement('button');
-          resetButton[textContent] = 'Restart';
-          document.body.appendChild(resetButton);
-          resetButton.addEventListener('click', function reset() {
-            resetButton.parentNode.removeChild(resetButton);
-            reStart();
-          });
-        },
-        domGameSubmitClick : function() {
-          var value = get_dom_v('.GameText', 'value') | 0;
-          set_dom_v('.GameText', 'value', '');
+      var guess = create([])
+      var turn = create(0)
+      var ran =  create(0)
+
+      var random =  _.curry(function(x) {return Math.floor(Math.random() * x) + 1})
+      var ranRandom = _.compose(setVal(ran), random())
+      var setTurn = setVal(turn)
+      var setGuess = setVal(guess)
+
+      var reStart = _.curry(function() {
+        setGuess([])
+        setTurn(0)
+        disabled(false)
+        ranRandom(100)
+        set_dom_v('.guess', 'textContent', '')
+        set_dom_v('.output', 'textContent', '')
+        set_dom_v('.confirm', 'textContent', '')
+      }) 
+      var setGameOver = function() {
+        disabled(true)
+        var button = create(document.createElement('button'))
+        _.set(getVal(button), 'textContent', 'Restart')
+        document.body.appendChild(getVal(button))
+        setListen(getVal(button), function reset() {
+          resetButton.parentNode.removeChild(getVal(button))
+          reStart()
+        })
+      }
+      var domGameSubmitClick = function() {
+        var value = get_dom_v('.GameText', 'value') | 0
+        set_dom_v('.GameText', 'value', '')
+        getVal(guess).push(value)
+
+        var str = 'Previous guesses: ' + getVal(guess).join(' ')
+        set_dom_v('.output', 'textContent', str)
     
-          guessArray.push(value);
-    
-          var str = 'Previous guesses: ' + guessArray.join(' ');
-          set_dom_v('.output', 'textContent', str);
-    
-          if (value === random) {
-            set_dom_v('.guess', 'textContent', 'Right!');
-            set_dom_Attribute('.guess', 'style', 'background-color:green;color:white;');
-            set_dom_v('.confirm', 'textContent', 'This is right!');
-            setGameOver();
-            return;
-          } else {
-            set_dom_v('.guess', 'textContent', 'Wrong!');
-            set_dom_Attribute('.guess', 'style', 'background-color:red;color:white;');
-            value > random ? set_dom_v('.confirm', 'textContent', 'Last guess was too height!') :
-            set_dom_v('.confirm', 'textContent', 'Last guess was too low!');
-          }
-    
-          turn++;
-          if (turn > 9) {
-            set_dom_v('.confirm', 'textContent', 'You lose!');
-            setGameOver();
-          }
+        if (value === getVal(ran)) {
+          set_dom_v('.guess', 'textContent', 'Right!')
+          set_dom_Attribute('.guess', 'style', 'background-color:green;color:white;')
+          set_dom_v('.confirm', 'textContent', 'This is right!')
+          setGameOver()
           return;
+        } else {
+          set_dom_v('.guess', 'textContent', 'Wrong!')
+          set_dom_Attribute('.guess', 'style', 'background-color:red;color:white;')
+          value > getVal(ran) ? set_dom_v('.confirm', 'textContent', 'Last guess was too height!') :
+          set_dom_v('.confirm', 'textContent', 'Last guess was too low!')
         }
-      };
+        setTurn(_.add(getVal(turn) + 1))
+        if (getVal(turn) > 9) {
+          set_dom_v('.confirm', 'textContent', 'You lose!')
+          setGameOver()
+        }
+        return
+      }
 
-      game.reStart();
-      get_dom('.GameSubmit').addEventListener('click', game.domGameSubmitClick);
+      var listen = setListen(domGameSubmitClick)
+
+      reStart()
+      listen(get_dom('.GameSubmit'))
     },
     characterStringCount : function() {
       var regEx = function(reg, str) {
